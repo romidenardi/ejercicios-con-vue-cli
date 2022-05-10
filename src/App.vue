7e6ec155dcbd2ed1de6046966a69dd10088bd9e4
romidenardi <rominadenardi@gmail.com>
2022-05-10 20:06:09 -0300

    ajustes indentado

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld :msg="msg"/>
-    <AppTittle :tittle="tittle"/>
-    <AppDescription :description="description"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld :msg="msg"/>
+        <AppTittle :tittle="tittle"/>
+        <AppDescription :description="description"/>
+    </div>
 </template>
 
 <script>
@@ -13,27 +13,27 @@ import AppTittle from './components/AppTittle.vue';
 import AppDescription from './components/AppDescription.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld, AppTittle, AppDescription,
-  },
-  data() {
-    return {
-      msg:"Esta es mi primera app con Vue CLI",
-      tittle:"Bienvenidos",
-      description:"Estamos trabajando con componentes."
+    name: 'App',
+    components: {
+        HelloWorld, AppTittle, AppDescription,
+    },
+    data() {
+        return {
+            msg:"Esta es mi primera app con Vue CLI",
+            tittle:"Bienvenidos",
+            description:"Estamos trabajando con componentes."
+        }
     }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>

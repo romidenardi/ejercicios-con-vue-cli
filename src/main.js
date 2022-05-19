0e1799deebe5b6a4ec904058cf3fc7ef98a8f80e
romidenardi <rominadenardi@gmail.com>
2022-05-18 22:29:40 -0300

    clase 10 bis

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router.js'
+import router from './router.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
